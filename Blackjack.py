657731a1b72255b44d279be6df4fc57504950bbc
MatthewKopelowitz <47190077+mattkopelowitz@users.noreply.github.com>
2024-04-05 15:10:48 -0700

    Added midGameMenu(), still need to code hit option and add auto dealer hits

--- Blackjack.py
+++ Blackjack.py
@@ -32,28 +32,49 @@ def runGame():
     dealerValue = dealCard(dealerValue, "Dealer")
     dealerValue = dealCard(dealerValue, "Dealer")
     
-    print("Your total hand value is: " + str(playerValue))
-    print("The dealer has a " + getCardName(dealerValue) + " showing")
+    print("Your total hand value is: " + str(playerValue) + "\n")
+    print("The dealer has a " + getCardName(dealerValue) + " showing\n")
+
+    midGameMenu(playerValue, dealerValue)
+
+def midGameMenu(handVal, dealerVal):
+    choice = 0
+    while choice != 1 and choice != 2: 
+        choice = input("What do you do? Type a number(1/2):\n1. Hit\n2. Stay\nChoice: ")
+        choice = int(choice)
+    if choice == 1:
+        pass
+    if choice == 2:
+        print("\nThe dealer reveals a total hand value of: " + str(dealerVal) + "\n")
+        if dealerVal > handVal:
+            print("The dealer is closer to 21, YOU LOSE")
+        elif dealerVal < handVal:
+            print("You are closer to 21, YOU WIN")
+        elif dealerVal == handVal:
+            print("You and the dealer PUSH")
+
     
 
 def main():
     gameNum = 1
     
-    print("Welcome To BlackJack!")
-    print("Game #" + str(gameNum))
+    print("Welcome To BlackJack!\n")
+    print("Game #" + str(gameNum) + "\n")
     
     playAgain = True
     while playAgain:
         runGame()
         validResponse = False
         while not validResponse:
-            response = input("Would you like to play again? (y/n): ")
+            response = input("\nWould you like to play again? (y/n): ")
             if response == "y":
+                print("\n")
                 playAgain = True
                 validResponse = True
                 gameNum += 1
                 print("Game #" + str(gameNum))
             elif response == "n":
+                print("\nThanks for playing!")
                 playAgain = False
                 validResponse = True
             else:
